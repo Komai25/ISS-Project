cfb2ea048ee2b93abb516fc28e0d56209eb5fd3d
k-alaya9 <mayalove314@gmail.com>
2024-11-22 14:11:52 +0300

    adding Asymmntic algorithm

--- client.py
+++ client.py
@@ -1,26 +1,63 @@
 import socket
-# from cryptography.fernet import Fernet
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
 from Crypto.Util.Padding import pad, unpad
+
+
+client_private_key = RSA.generate(2048)
+client_public_key = client_private_key.publickey()
+print(f"Client Public Key:\n{client_public_key.export_key().decode()}")
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(('localhost', 12345))
 
-key = client_socket.recv(1024)
-print(f"Recevied key from server. Key received: {key.hex()}") #key.hex()
+
+server_public_key = RSA.import_key(client_socket.recv(1024))
+print("Received Server's Public Key.")
+
+
+client_socket.send(client_public_key.export_key())
+
+
+mode = input("Choose encryption mode (AES/RSA/None): ")
+client_socket.send(mode.encode())
+
+if mode == "AES":
+
+    key = client_socket.recv(32)
+    print("Received AES key from server.")
+elif mode == "RSA":
+    print("RSA mode selected.")
+elif mode == "None":
+    print("No encryption selected.")
 
 while True:
+
     message = input("Client: ")
-    cipher = AES.new(key,AES.MODE_CBC)
-    encrypted_message = cipher.iv + cipher.encrypt(pad(message.encode(),AES.block_size))
+    if mode == "AES":
+        cipher = AES.new(key, AES.MODE_CBC)
+        encrypted_message = cipher.iv + cipher.encrypt(pad(message.encode(), AES.block_size))
+    elif mode == "RSA":
+        cipher_rsa = PKCS1_OAEP.new(server_public_key)
+        encrypted_message = cipher_rsa.encrypt(message.encode())
+    elif mode == "None":
+        encrypted_message = message.encode()
+
     client_socket.send(encrypted_message)
 
 
-    encrypted_response = client_socket.recv(1024)
-    iv = encrypted_response[:16]
-    encrypted_data = encrypted_response[16:]
-    cipher = AES.new(key, AES.MODE_CBC,iv)
-    decrypted_response = unpad(cipher.decrypt(encrypted_data),AES.block_size).decode()
+    data = client_socket.recv(1024)
+    if mode == "AES":
+        iv = data[:16]
+        encrypted_data = data[16:]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        decrypted_response = unpad(cipher.decrypt(encrypted_data), AES.block_size).decode()
+    elif mode == "RSA":
+        cipher_rsa = PKCS1_OAEP.new(client_private_key)
+        decrypted_response = cipher_rsa.decrypt(data).decode()
+    elif mode == "None":
+        decrypted_response = data.decode()
+
     print(f"Server: {decrypted_response}")
 
 client_socket.close()
--- server.py
+++ server.py
@@ -1,48 +1,70 @@
 import socket
-# from cryptography.fernet import Fernet
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
 from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
 
 
+server_private_key = RSA.generate(2048)
+server_public_key = server_private_key.publickey()
+print(f"Server Public Key:\n{server_public_key.export_key().decode()}")
 
-key = get_random_bytes(32)
-print(f'Server Key: {key.hex()}')
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 12345))
 server_socket.listen(1)
-
-print(f"Server is running... Key: {key.hex()}")
+print("Server is running...")
 
 conn, addr = server_socket.accept()
 print(f"Connection established with {addr}")
 
+conn.send(server_public_key.export_key())
 
-# Send the Key to the Client
-conn.send(key)
+client_public_key = RSA.import_key(conn.recv(1024))
+print("Received Client's Public Key.")
+
+mode = conn.recv(1024).decode()
+print(f"Client selected mode: {mode}")
+
+if mode == "AES":
+    key = get_random_bytes(32)
+    conn.send(key)
+elif mode == "RSA":
+    print("RSA mode selected.")
+elif mode == "None":
+    print("No encryption selected.")
 
 while True:
-    # recive message 
-    encrypted_message = conn.recv(1024)
-    if not encrypted_message:
+    # Receive encrypted message from Client
+    data = conn.recv(1024)
+    if not data:
         break
-    
-    # Decrypt message
-    iv = encrypted_message[:16]
-    encrypted_data = encrypted_message[16:]
-    cipher = AES.new(key, AES.MODE_CBC,iv)
-    decrypted_message = unpad(cipher.decrypt(encrypted_data),AES.block_size).decode()
-    print (f"Client: {decrypted_message}")
 
-    #Send message 
+    if mode == "AES":
+        iv = data[:16]
+        encrypted_data = data[16:]
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        decrypted_message = unpad(cipher.decrypt(encrypted_data), AES.block_size).decode()
+    elif mode == "RSA":
+        cipher_rsa = PKCS1_OAEP.new(server_private_key)
+        decrypted_message = cipher_rsa.decrypt(data).decode()
+    elif mode == "None":
+        decrypted_message = data.decode()
+
+    print(f"Client: {decrypted_message}")
+
+    # Send Response to Client
     response = input("Server: ")
-    cipher = AES.new(key, AES.MODE_CBC)
-    iv = cipher.iv
-    encrypted_response = iv + cipher.encrypt(pad(response.encode(), AES.block_size))
-    conn.send(encrypted_response)
+    if mode == "AES":
+        cipher = AES.new(key, AES.MODE_CBC)
+        encrypted_response = cipher.iv + cipher.encrypt(pad(response.encode(), AES.block_size))
+    elif mode == "RSA":
+        cipher_rsa = PKCS1_OAEP.new(client_public_key)
+        encrypted_response = cipher_rsa.encrypt(response.encode())
+    elif mode == "None":
+        encrypted_response = response.encode()
 
-    
+    conn.send(encrypted_response)
 
 conn.close()
 server_socket.close()
